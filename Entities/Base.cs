34c647ff9bc728545b67f6183630223b4c39f615
ısmayıl abbasov <abbasov98@hotmail.com>
2025-11-01 15:38:31 +0300

    Add project files.

---
+++ Entities/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Entities
+{
+    public  class Base
+    {
+        public string UnitPrice { get; set; }
+    }
+}
